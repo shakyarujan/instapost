48ba564a724cc3481dd73e47514bc612062b393d
Rujan shakya <rujanshakya76@gmail.com>
2016-11-18 14:03:08 +0000

    Setting Up Validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
     def new
       @post = Post.new
     end
+    
+    def create
+        @post = current_user.posts.create(post_params)
+        if @post.valid?
+        redirect_to root_path
+        else
+        render :new, status: :unprocessable_entity
+        end
+    end
+    
+    private
+    
+    def post_params
+        params.require(:post).permit(:user_id, :photo, :description)
+    end
 end
